d759b7408b140405aece2fbc7415e281a95bbcdb
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-03-13 17:01:07 +0000

    updated files

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.";
 }
 
-// change background color to lime:
-function changeBodyBg(color){
-  document.getElementById("body").style.background.color = "lime";
-}
-
-// change background color to orange:
-function changeBodyBg(color){
-  document.getElementById("body").style.background = color;
-}
-
 // change background color to orange:
 function changeBodyBg(color){
   document.body.style.backgroundColor = "lime";
